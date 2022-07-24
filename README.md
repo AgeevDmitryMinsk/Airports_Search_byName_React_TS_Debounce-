077636d45bc11d9230c29cd80103552bb9077bc3
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-07-25 01:37:44 +0300

    add README.md link at gh-pages

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ## Система для просмотра открытых данных по аэропортам
-# Swagger:
-
+### Swagger:
 http://docker.digital-spectr.ru:8888/api/swagger/
+
+#
+#
+для запуска проекта перейдите по ссылке:
+https://ageevdmitryminsk.github.io/Airports_Search_byName_React_TS_Debounce-/
--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 //console.log(process.env.REACT_APP_BASE_URL) // undefined
 
 export default axios.create({
+	// изменил в ручную http -> https но в таком случае получаю ошибку network error, вернул обратно на http
 	baseURL: "http://docker.digital-spectr.ru:8888/api/"
+
+	//baseURL: "http://docker.digital-spectr.ru:8888/api/"
 	// baseURL: process.env.REACT_APP_BASE_URL - можно и так, но требуется доп.настройка среды окружения
 
 })
